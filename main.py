72bd195363756b2c5da66c6aaea7bfe1216d1ba9
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2024-09-12 09:01:06 -0300

    configuração inicial do projeto

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Ola mundo"
+    return render_template("base.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
