39349f21c215dca6ad30e080b5022f522caecce6
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2024-09-14 11:46:07 -0300

    Adicionando telas de login e register

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
